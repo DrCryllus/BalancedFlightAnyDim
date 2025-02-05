6174d89c15088b239dd97c1094a43791b1ce6d3c
DrCryllus <43461802+DrCryllus@users.noreply.github.com>
2025-02-04 23:46:53 -0800

    Update FlightController.java

--- src/main/java/com/vice/balancedflight/content/flightAnchor/FlightController.java
+++ src/main/java/com/vice/balancedflight/content/flightAnchor/FlightController.java
@@ -97,7 +97,7 @@ public class FlightController
     private static boolean IsWithinFlightRange(Player player)
     {
         if (player.level.dimension() != Level.OVERWORLD)
-            return false;
+            return true;
 
         double anchorDistanceMultiplier = BalancedFlightConfig.anchorDistanceMultiplier.get();
 
